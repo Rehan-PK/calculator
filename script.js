63f68ab58b006a4949c1d42700f2f20c044ce6cd
rehan-official <rehanaliofficial>
2023-12-13 16:59:39 +0500

    working calculator 13-Dec

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// Calculator object constructor
-function Calculator () {
-  this.methods = {
-    "+": (a, b) => a + b,
-    "-": (a, b) => a - b,
-    "*": (a, b) => a * b,
-    "/": (a, b) => a / b,
-  };
-
-  this.calculate = function(str) {
-    let splitted = str.split(" "),
-      a = +splitted[0],
-      op = splitted[1],
-      b = +splitted[2];
-    
-    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
-    }
-
-    return this.methods[op](a, b);
-  };
-
-  // this.addMethod = (name, func) => this.methods[name] = func;  
-}
-
 // 1. Making functions + - * /
 function add(a, b) {
   return a + b;
@@ -45,7 +20,8 @@ function divide(a, b) {
 let a = [], 
 b = [], 
 op, 
-result;
+result,
+decimal;
 
 // 3. creating function operate
 function operate(a, op, b) {
@@ -79,106 +55,12 @@ function operate(a, op, b) {
 //      You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
 //      This is the hardest part of the project. You need to figure out how to store all the values and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
 
-// -------------------1ST ATTEMPT-------------------
-// let keyValue = buttons[i].innerHTML.trim();
-// if (isNumber(keyValue)) {
-//   a.push(keyValue);
-//   display = a.join("");
-//   console.log(display);
-// } else if (keyValue == '+' || keyValue == '-' || keyValue == '*' || keyValue == '/') {
-//   op = keyValue;
-// } else {
-//   return false;
-// }
-
-// -------------------2ND ATTEMPT-------------------
-// let buttons = document.querySelectorAll(".key");
-// for (let i = 0; i < buttons.length; i++) {
-//   buttons[i].addEventListener("click", function () {
-//     let keyValue = buttons[i].innerHTML.trim();
-//     if (Number.isInteger(Number(keyValue))) {
-//       a.push(keyValue);
-//       display = a.join("");
-//       console.log(display);
-//     } else if (keyValue == '+' || keyValue == '-' || keyValue == '*' || keyValue == '/') {
-//       op = keyValue;
-//       console.log(op);
-//     } else {
-//       console.log('neither a number nor a symbol');
-//     }
-//   }
-//   )
-// }
-
-// ---------------- CORRECT ---------------------
 // Select all buttons of key class
 let buttons = document.querySelectorAll(".key");
 // add click eventlistener for all keys, check for input of numbers/operators
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => checkInput(i));
 }
-// function checkInput(key) {
-//   let keyValue = buttons[key].innerHTML.trim();  
-//   if (Number.isInteger(Number(keyValue))) {
-//     a.push(keyValue);
-//     display = a.join("");
-//     console.log(display);
-//     return a;
-//   } else if (keyValue == '+' || keyValue == '-' || keyValue == '*' || keyValue == '/') {
-//     op = keyValue;
-//     console.log(op);
-//     return op;
-//   } else {
-//     return console.log('neither a number nor an arithmetic operator');
-//   }
-// }
-
-// function checkInput(key) {
-//   let keyValue = buttons[key].innerHTML.trim();
-//   let checkForInt = Number.isInteger(Number(keyValue));
-//   if (checkForInt && !op) {
-//     a.push(keyValue);
-//     console.log(a);
-//   } else if (checkForInt && op) {
-//     b.push(keyValue);
-//     console.log(b);
-//   } else if (!checkForInt && !op) {
-//     op = keyValue;
-//     console.log(op);
-//   } else if (keyValue == "=" && a && b && op) {
-//     let result = operate(Number(a.join("")), op, Number(b.join("")));
-//     a.splice(0, a.length);
-//     b.splice(0, b.length);
-//     op = null;
-//     console.log(result);
-//     return result;
-//   } else if (keyValue == 'CLEAR') {
-//     if (b.length > 0) {
-//       b.splice(0, a.length);
-//       a.splice(0, a.length);
-//       op = null;
-//     } else if (op) {
-//       op = null;
-//       a.splice(0, b.length);
-//     } else if (a.length > 0) {
-//       a.splice(0, a.length);
-//     }
-//   } else if (keyValue == 'DELETE') {
-//     if (op) {
-//       if (b.length > 0) {
-//         b.pop();
-//       } else {
-//         op = null;
-//       }
-//     } else if (!op && a.length > 0) {
-//       a.pop();
-//     }
-//   } else {
-//     alert('neither a number nor an arithmetic operator');
-//   }
-// }
-
-// pressing button clear makes op = clear, check it
 
 // converting above to switch statement
 function checkInput(key) {
@@ -194,7 +76,7 @@ function checkInput(key) {
     case "8":
     case "9":
     case "0":
-      pushKey(keyValue);
+      pushNumber(keyValue);
       updateBigDisplay();
       // console.log(keyValue);
       break;
@@ -203,45 +85,21 @@ function checkInput(key) {
       updateBigDisplay();
       break;
     case "DELETE":
-      deleteIt(keyValue);
+      deleteIt();
       updateBigDisplay();
       break;
     case "+":
     case "-":
     case "*":
     case "/":
-      // add below conditional here
-      // if op already selected & a & b then execute operate function else 
-      if (op) {
-        if (a.length > 0 && b.length > 0) {
-          result = operate(Number(a.join("")), op, Number(b.join("")))
-          console.log(result);
-          op = keyValue;
-          updateBigDisplay();
-          // make result equal to a
-          // set op to the current keyValue
-          // set b to 0
-          a.splice(0, a.length, result);
-          b.splice(0, b.length);
-          result = null;
-        } else if (a.length > 0 && b.length == 0) {
-          op = keyValue;
-          updateBigDisplay();
-        }
-      } else if (result) {
-        result = a;
-        op = keyValue;
-        updateBigDisplay(); 
-      } else {
-        op = keyValue;
-        updateBigDisplay();
-      }
+      pushOperator(keyValue);
       break;
     case "=":
       // 3 scenarios
       // 1. if a && !op && !b: then result = a & updateDisplay
       if (result) {
         // do nothing
+        updateBigDisplay();
       } else if (a && !op && !b) {
         result = a;
         updateBigDisplay();
@@ -250,6 +108,8 @@ function checkInput(key) {
         result = a;
         updateBigDisplay();
         // 3. if a && op && b: then same as below
+      } else if (!a && !op && !b) {
+        updateBigDisplay();
       } else {
         result = operate(Number(a.join("")), op, Number(b.join("")))
         a.splice(0, a.length);
@@ -262,28 +122,40 @@ function checkInput(key) {
   }
 }
 
-function pushKey(keyValue) {
+// check for existing variables (a,op,b,result) then decide about key operation
+function pushNumber(keyValue) {
   let checkForInt = Number.isInteger(Number(keyValue));
-  if (op && checkForInt) {
+  if (result && checkForInt) {
+    clearIt();
+    a.push(keyValue);
+  } else if (a && op && checkForInt) {
     b.push(keyValue);
-    console.log(b);
   } else if (!op && checkForInt) {
     a.push(keyValue);
-    console.log(a);
+  }
+}
+
+function pushOperator(keyValue) {
+  if (result) {
+    op = keyValue;
+    a.splice(0, a.length, result);
+    b.splice(0, b.length);
+    result = null;
+    updateBigDisplay();
+  } else if (a.length > 0 && op && b.length > 0) {
+    result = operate(Number(a.join("")), op, Number(b.join("")));
+    a.splice(0, a.length, result);
+    op = keyValue;
+    updateBigDisplay();
+    b.splice(0, b.length);
+    result = null;
+  } else {
+    op = keyValue;
+    updateBigDisplay();
   }
 }
 
 function clearIt() {
-  // if (b.length > 0) {
-  //   b.splice(0, a.length);
-  //   a.splice(0, a.length);
-  //   op = null;
-  // } else if (op) {
-  //   op = null;
-  //   a.splice(0, b.length);
-  // } else if (a.length > 0) {
-  //   a.splice(0, a.length);
-  // }
   a.splice(0, a.length);
   b.splice(0, b.length);
   op = null;
@@ -291,51 +163,33 @@ function clearIt() {
 }
 
 function deleteIt() {
-  // if (op) {
-  //   if (b.length > 0) {
-  //     b.pop();
-  //   } else {
-  //     op = null;
-  //   }
-  // } else if (!op && a.length > 0) {
-  //   a.pop();
-  // }
-
   if (result) {
     result = null;
-  } else if (b) {
+  } else if (b.length > 0) {
     b.pop();
   } else if (op) {
     op = null;
-  } else if (a) {
+  } else if (a.length > 0) {
     a.pop();
   }
 }
 
 function updateBigDisplay() {
-  let bigDisplay = document.querySelector(".live-display.big").textContent;
-  // if (result && op) {
-  //   bigDisplay.textContent = `${result + op}`;
-  // } else if (result && !op) {
-  //   bigDisplay.textContent = `${result}`;
-  // } else {
-  //   bigDisplay.textContent = `${(a.join("") ? a.join("") : '') 
-  //   + (op ? op : '') 
-  //   + (b.join("") ? b.join("") : '')}`;
-  // }
-
-  if (result && op) {
-    bigDisplay = `${result + op}`;
-  } else if (result && !op) {
-    bigDisplay = `${result}`;
+  let bigDisplay = document.querySelector(".live-display.big");
+  if (result && !op) {
+    bigDisplay.textContent = `${result}`;
+  } else if (result && op) {
+    bigDisplay.textContent = `${result + op}`;
   } else {
-    bigDisplay = `${(a.join("") ? a.join("") : '') 
+    bigDisplay.textContent = `${(a.join("") ? a.join("") : '') 
       + (op ? op : '') 
       + (b.join("") ? b.join("") : '')}`;
   }
 }
 
-function updateSmallDisplay() {
-  let smallDisplay = document.querySelector(".live-display small");
-}
+// Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5. It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)
+
+
+
+// Add keyboard support! You might run into an issue where keys such as (/) might cause you some trouble. Read the MDN documentation for event.preventDefault to help solve this problem.
 
